adbc90f9d05f96e12b804c168273bf511b464296
whitewater design <whitewaterdesign>
2025-09-22 18:15:58 +0100

    fix: DF-378 - Fix bug in human v1 list display

---
+++ src/service/mappers/formatters/machine/v1.test.js
@@ -0,0 +1,87 @@
+import { buildDefinition } from '@defra/forms-model/stubs'
+
+import {
+  exampleNotifyFormDefinition,
+  exampleNotifyFormMessage
+} from '~/src/service/mappers/formatters/__stubs__/notify.js'
+import { getFormatter } from '~/src/service/mappers/formatters/index.js'
+
+jest.mock('nunjucks', () => {
+  const environment = {
+    addFilter: jest.fn(),
+    addGlobal: jest.fn()
+  }
+  return {
+    configure: jest.fn(() => environment)
+  }
+})
+jest.mock('~/src/config/index.js', () => ({
+  config: {
+    get: jest.fn(() => {
+      return 'http://designer'
+    })
+  }
+}))
+
+describe('Page controller helpers', () => {
+  beforeAll(() => {
+    jest.useFakeTimers().setSystemTime(new Date('2025-09-01T00:00:00Z'))
+  })
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
+  it('should return a valid machine v1 response', () => {
+    const definition = buildDefinition({
+      ...exampleNotifyFormDefinition,
+      output: {
+        audience: 'machine',
+        version: '1'
+      }
+    })
+    const formatter = getFormatter('machine', '1')
+    const formatted = JSON.parse(
+      formatter(exampleNotifyFormMessage, definition, '1')
+    )
+    expect(formatted).toEqual({
+      meta: {
+        schemaVersion: '1',
+        timestamp: expect.any(String),
+        referenceNumber: '874-C7C-D60',
+        definition: {
+          ...exampleNotifyFormDefinition,
+          output: { audience: 'machine', version: '1' }
+        }
+      },
+      data: {
+        main: {
+          JHCHVE: 'Someone',
+          hTFiWF: '1 Anywhere Street,Anywhereville,Anywhereshire,AN1 2WH',
+          zznFWF: '2000-01-01',
+          KGSRJU: '2025-08',
+          hVcHQv: 'Gandalf,Frodo'
+        },
+        repeaters: {
+          repeaterOptionName: [
+            {
+              repeaterComponentName: 'Frodo'
+            },
+            {
+              repeaterComponentName: 'Gandalf'
+            }
+          ]
+        },
+        files: {
+          IWEgMu: [
+            {
+              fileName: 'supporting_evidence.pdf',
+              fileId: 'ef4863e9-7e9e-40d0-8fea-cf34faf098cd',
+              userDownloadLink:
+                'http://localhost:3005/file-download/ef4863e9-7e9e-40d0-8fea-cf34faf098cd'
+            }
+          ]
+        }
+      }
+    })
+  })
+})
---
+++ src/service/mappers/formatters/machine/v2.test.js
@@ -0,0 +1,88 @@
+import { buildDefinition } from '@defra/forms-model/stubs'
+
+import {
+  exampleNotifyFormDefinition,
+  exampleNotifyFormMessage
+} from '~/src/service/mappers/formatters/__stubs__/notify.js'
+import { getFormatter } from '~/src/service/mappers/formatters/index.js'
+
+jest.mock('nunjucks', () => {
+  const environment = {
+    addFilter: jest.fn(),
+    addGlobal: jest.fn()
+  }
+  return {
+    configure: jest.fn(() => environment)
+  }
+})
+jest.mock('~/src/config/index.js', () => ({
+  config: {
+    get: jest.fn(() => {
+      return 'http://designer'
+    })
+  }
+}))
+
+describe('Page controller helpers', () => {
+  beforeAll(() => {
+    jest.useFakeTimers().setSystemTime(new Date('2025-09-01T00:00:00Z'))
+  })
+  afterAll(() => {
+    jest.useRealTimers()
+  })
+
+  it('should return a valid machine v2 response', () => {
+    const definition = buildDefinition({
+      ...exampleNotifyFormDefinition,
+      output: {
+        audience: 'machine',
+        version: '2'
+      }
+    })
+    const formatter = getFormatter('machine', '2')
+    expect(
+      JSON.parse(formatter(exampleNotifyFormMessage, definition, '2'))
+    ).toEqual({
+      meta: {
+        schemaVersion: '2',
+        timestamp: expect.any(String),
+        definition,
+        referenceNumber: '874-C7C-D60'
+      },
+      data: {
+        main: {
+          JHCHVE: 'Someone',
+          hTFiWF: {
+            addressLine1: '1 Anywhere Street',
+            town: 'Anywhereville',
+            county: 'Anywhereshire',
+            postcode: 'AN1 2WH'
+          },
+          zznFWF: { day: 1, month: 1, year: 2000 },
+          KGSRJU: { month: 8, year: 2025 },
+          hVcHQv: ['Gandalf', 'Frodo']
+        },
+        repeaters: {
+          repeaterOptionName: [
+            {
+              repeaterComponentName: 'Frodo'
+            },
+            {
+              repeaterComponentName: 'Gandalf'
+            }
+          ]
+        },
+        files: {
+          IWEgMu: [
+            {
+              fileName: 'supporting_evidence.pdf',
+              fileId: 'ef4863e9-7e9e-40d0-8fea-cf34faf098cd',
+              userDownloadLink:
+                'http://localhost:3005/file-download/ef4863e9-7e9e-40d0-8fea-cf34faf098cd'
+            }
+          ]
+        }
+      }
+    })
+  })
+})
