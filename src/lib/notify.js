3e216d2c8de2898fe6c89945f15282b950294728
Jez Barnsley <jbarnsley.github@gmail.com>
2026-01-07 15:28:36 +0000

    Escape general answer text

--- src/lib/notify.js
+++ src/lib/notify.js
@@ -49,33 +49,11 @@ export function escapeNotifyContent(str) {
 }
 
 /**
- * Prevent Markdown formatting
+ * Prevent Markdown formatting by marking content as a 'code block'
  * @param {string} answer
  */
 export function escapeMarkdownChars(answer) {
-  const punctuation = new Map([
-    ['`', '&grave;'],
-    ["'", '&apos;'],
-    ['*', '&ast;'],
-    ['_', '&lowbar;'],
-    ['{', '&lbrace;'],
-    ['}', '&rbrace;'],
-    ['[', '&lbrack;'],
-    [']', '&rbrack;'],
-    ['(', '&lpar;'],
-    [')', '&rpar;'],
-    ['#', '&num;'],
-    ['+', '&plus;'],
-    [' - ', ' &hyphen; '],
-    ['.', '&period;'],
-    ['!', '&excl;']
-  ])
-
-  for (const [charOrig, charRepl] of punctuation.entries()) {
-    answer = answer.replaceAll(charOrig, charRepl)
-  }
-
-  return answer
+  return `\`\`\`\r\n${answer}\r\n\`\`\``
 }
 
 /**
