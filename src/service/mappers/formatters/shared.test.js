44ee15245b7e7ed79ad6222ef8fa13060871335e
Jez Barnsley <jbarnsley.github@gmail.com>
2026-02-06 11:01:16 +0000

    Fixed tests

--- src/service/mappers/formatters/shared.test.js
+++ src/service/mappers/formatters/shared.test.js
@@ -1,23 +1,27 @@
+import { buildFormAdapterSubmissionMessage } from '~/src/service/__stubs__/event-builders.js'
 import { extractPaymentDetails } from '~/src/service/mappers/formatters/shared.js'
 
 describe('extractPaymentDetails', () => {
-  const baseMessage = /** @type {FormAdapterSubmissionMessage} */ ({
-    data: {
-      payment: {
-        paymentId: 'payment-id',
-        reference: 'ABC-DEF-123',
-        amount: 17.5,
-        description: 'Pay for a licence',
-        createdAt: '2026-05-02T09:21:55'
+  const baseMessage = buildFormAdapterSubmissionMessage(
+    /** @type {FormAdapterSubmissionMessage} */ ({
+      data: {
+        payment: {
+          paymentId: 'pay_abc123',
+          reference: 'REF-123-456',
+          amount: 145.5,
+          description: 'Application fee',
+          createdAt: '2025-11-10T17:01:29.000Z'
+        }
       }
-    }
-  })
+    })
+  )
+
   it('should format amount and date/time', () => {
     const message = structuredClone(baseMessage)
     expect(extractPaymentDetails(message)).toEqual({
-      amount: '£17.50',
-      description: 'Pay for a licence',
-      dateOfPayment: '9:21am on 2 May 2026'
+      amount: '£145.50',
+      description: 'Application fee',
+      dateOfPayment: '5:01pm on 10 November 2025'
     })
   })
 
@@ -26,12 +30,12 @@ describe('extractPaymentDetails', () => {
     // @ts-expect-error - will be defined for testing
     message.data.payment.amount = 100
     // @ts-expect-error - will be defined for testing
-    message.data.payment.createdAt = '2026-05-12T14:21:55'
+    message.data.payment.createdAt = '2026-12-02T14:21:55.000Z'
 
     expect(extractPaymentDetails(message)).toEqual({
       amount: '£100.00',
-      description: 'Pay for a licence',
-      dateOfPayment: '2:21pm on 12 May 2026'
+      description: 'Application fee',
+      dateOfPayment: '2:21pm on 2 December 2026'
     })
   })
 
@@ -42,8 +46,8 @@ describe('extractPaymentDetails', () => {
 
     expect(extractPaymentDetails(message)).toEqual({
       amount: '£123,456.78',
-      description: 'Pay for a licence',
-      dateOfPayment: '9:21am on 2 May 2026'
+      description: 'Application fee',
+      dateOfPayment: '5:01pm on 10 November 2025'
     })
   })
 })
