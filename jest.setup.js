f5ef8e46086985fa77ea1c4b8bf59ac5e69610e3
Jez Barnsley <114290619+jbarnsley10@users.noreply.github.com>
2026-03-24 16:49:48 +0000

    Merge pull request #122 from DEFRA/feat/df-912-dlq
    Feat/df 912: Can view messages and redrive the dead-letter queue

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,7 @@
 process.env.MANAGER_URL = 'http://manager'
 process.env.DESIGNER_URL = 'http://designer'
+process.env.OIDC_JWKS_URI =
+  'https://login.microsoftonline.com/770a2450-0227-4c62-90c7-4e38537f1102/discovery/v2.0/keys'
 process.env.NOTIFY_TEMPLATE_ID = '2d48d7f9-32ae-43be-9bf1-8bf5cd7bfc17'
 process.env.NOTIFY_REPLY_TO_ID = '462e5ac0-28f1-4e51-9d8e-8d8c31f59f6a'
 process.env.NOTIFY_API_KEY = 'dummy'
