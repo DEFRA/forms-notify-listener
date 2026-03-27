75e65739f17daa4280ea6a01b5ef17de91685555
Parvesh M <parveshh@gmail.com>
2026-03-27 14:48:51 +0000

    fix: add environment configuration for tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,13 @@
 process.env.MANAGER_URL = 'http://manager'
 process.env.DESIGNER_URL = 'http://designer'
+process.env.PORT = '3006'
+process.env.CDP_HTTPS_PROXY = ''
+process.env.ENTITLEMENT_URL = 'http://entitlements'
+process.env.SQS_ENDPOINT = 'http://localhost:4566'
+process.env.EVENTS_SQS_QUEUE_URL =
+  'http://sqs.eu-west-2.127.0.0.1:4566/000000000000/forms_notify_listener_events'
+process.env.EVENTS_SQS_DLQ_ARN =
+  'arn:aws:sqs:eu-west-2:000000000000:forms_notify_listener_events-deadletter'
 process.env.OIDC_JWKS_URI =
   'https://login.microsoftonline.com/770a2450-0227-4c62-90c7-4e38537f1102/discovery/v2.0/keys'
 process.env.NOTIFY_TEMPLATE_ID = '2d48d7f9-32ae-43be-9bf1-8bf5cd7bfc17'
