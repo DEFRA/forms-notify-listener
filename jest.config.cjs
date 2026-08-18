5a670318ac2ab5d7f0551e3919badf12bb65d3cb
Aaron Scully <aaron@scullyon.com>
2026-08-18 09:34:38 +0100

    Config update

--- jest.config.cjs
+++ jest.config.cjs
@@ -43,6 +43,7 @@ module.exports = {
       '@defra/forms-model/.*',
       'nanoid', // Supports ESM only
       'slug', // Supports ESM only
+      'geodesy', // Supports ESM only
       '@defra/hapi-tracing', // Supports ESM only
       '@defra/forms-engine-plugin'
     ].join('|')}/)`
